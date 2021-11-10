792edfb90ac9269158db64ef295ef1f79cba2234
Azurei0 <43456427+Azurei0@users.noreply.github.com>
2021-11-11 00:09:00 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -7,28 +7,8 @@
  */
 
  import React, {Component} from "react";
- import {StyleSheet, Text, View, TextInput, Button} from "react-native";
+ import {StyleSheet, Text, View, TextInput, Button, useColorScheme} from "react-native";
   
-//    render() { 
-//      return ( 
-//        <View style={styles.container}> 
-//          <Text style={styles.header}>My Biodata</Text> 
-//          <Text style={styles.contents}>My name is: {this.state.name}</Text>  
-//          <Text style={styles.contents}>Address: {this.state.address}</Text>
-//          <Text style={styles.counter}onPress={() => this.updateCount()}> {this.state.count} </Text> 
-//          <Text style={styles.contents}onPress={() => this.setState({count: 0})}> Reset</Text>  
-//        </View> 
-//      );  
-//    } 
-//  } 
-    
- /**
-  * Sample React Native App
-  * https://github.com/facebook/react-native
-  *
-  * @format
-  * @flow strict-local
-  */ 
 
   export default class App extends Component{
 
@@ -45,7 +25,7 @@
     
     updateCount(){ 
       var inputArray = this.state.wordInput.split('');
-      this.setState({totalCharacter: this.state.totalCharacter=0, consonantCount: this.state.consonantCount= 0, vowelCount: this.state.vowelCount= 0});
+      this.setState({totalCharacter: 0, consonantCount: this.state.consonantCount= 0, vowelCount: this.state.vowelCount= 0});
       this.setState({totalCharacter: inputArray.length});
       for(var i=0;i < inputArray.length; i++){
         
@@ -60,22 +40,62 @@
   
     render(){
       return(
-        <View>
-              <Text>Word Analyzer 2000</Text>
-              <Text>Word Input</Text>
-              <TextInput
+        <View style={styles.container}>
+              <Text style={styles.apptitle}>Word Analyzer 2000</Text>
+              <Text style={styles.prompt}>Word Input</Text>
+              <TextInput style={styles.inputstyle}
                 //defaultValue='Type Here!'
-                placeholder='Type Here'
+                placeholder='Type Here!'
                 onChangeText={(text) => this.setState({wordInput: text})}
                 />
               <Button title='Analyze!' onPress={() => this.updateCount()}/>
-              <Text>Word: {this.state.wordInput}</Text>
-              <Text>Vowels: {this.state.vowelCount}</Text>
-              <Text>Consonants: {this.state.consonantCount}</Text>
-              <Text>No of Characters: {this.state.totalCharacter}</Text>
+              <Text style={styles.output}>Word: {this.state.wordInput}</Text>
+              <Text style={styles.output}>Vowels: {this.state.vowelCount}</Text>
+              <Text style={styles.output}>Consonants: {this.state.consonantCount}</Text>
+              <Text style={styles.output}>No of Characters: {this.state.totalCharacter}</Text>
         </View>
       );
     }
   }
-  
-  //export default App;
+
+  const styles = StyleSheet.create({
+    container: {
+      borderColor: 'blue',
+      borderWidth: 2,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: '#D6D5D5',
+    },
+
+    apptitle: {
+      padding: 25,
+      color: '#2716C2',
+      fontSize: 35,
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+    },
+    
+    prompt: {
+      fontSize: 28,
+      color: 'green',
+      padding: 15,
+    },
+
+    inputstyle: {
+      padding: 25,
+      color: 'black',
+      borderWidth: 2,
+      borderColor: 'red',
+      margin: 25,
+    },
+
+    output: {
+      padding: 20,
+      justifyContent: 'space-evenly',
+      alignItems: 'flex-start',
+      fontSize: 20,
+
+    }
+    
+  })
